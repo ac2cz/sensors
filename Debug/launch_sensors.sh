e8eeddf098f75a0a2ed79b6877f9bbf3fa6f9011
Chris Thompson <g0kla@arrl.net>
2025-12-02 09:35:25 -0500

    Update launch scropt

--- Debug/launch_sensors.sh
+++ Debug/launch_sensors.sh
@@ -5,11 +5,11 @@
 #
 LOGFILE=sensor_data
 
-export PATH="/mnt/usb-disk/ariss/bin:/home/pi/bin:$PATH"
-export LD_LIBRARY_PATH=/mnt/usb-disk/ariss/lib:/usr/local/lib/iors_common:$LD_LIBRARY_PATH
+export PATH="/ariss/bin:/home/pi/bin:$PATH"
+export LD_LIBRARY_PATH=/ariss/lib:/usr/local/lib/iors_common:$LD_LIBRARY_PATH
 
 echo Launching sensors
 
 echo LD_LIBRARY_PATH: $LD_LIBRARY_PATH
 
-./sensors -f $LOGFILE -p 30 -v 
+./sensors -d /ariss -v 
